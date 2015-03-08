f12bf9590aa3ec9b5e7297d0ffdb9881261c2114
Adam Calihman <adamcalihman@gmail.com>
2015-03-08 04:56:25 +0000

    add validations to fields preventing blanks

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 }, #max 140 character limit
+                              presence: true #validation for the presence of micropost content in order to ensure that microposts can’t be blank
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :microposts
+  validates FILL_IN, presence: true #presence  (of user name) validations to the User model
+  validates FILL_IN, presence: true #presence (of user email) validations to the User model
 end
