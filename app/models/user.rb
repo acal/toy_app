686979788fcd0f16661a9d066fa807024a846844
Adam Calihman <adamcalihman@gmail.com>
2015-03-08 05:00:20 +0000

    debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :microposts
-  validates FILL_IN, presence: true #presence  (of user name) validations to the User model
-  validates FILL_IN, presence: true #presence (of user email) validations to the User model
+  validates FILL_IN, presence: true 
+  #presence  (of user name) validations to the User model
+  validates FILL_IN, presence: true 
+  #presence (of user email) validations to the User model
 end
