71a530642b59825f1a153b92401aa4052432cdf5
Adam Calihman <adamcalihman@gmail.com>
2015-03-08 05:02:20 +0000

    undo bad debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ActiveRecord::Base
   has_many :microposts
-  validates FILL_IN, presence: true 
-  #presence  (of user name) validations to the User model
-  validates FILL_IN, presence: true 
-  #presence (of user email) validations to the User model
 end
