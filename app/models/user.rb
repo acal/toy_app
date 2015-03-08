2e07a8f30da0c8ba60a8bd3195c141922d7dc10d
Adam Calihman <adamcalihman@gmail.com>
2015-03-08 19:06:16 +0000

    fixed email validation and added new username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :microposts
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
