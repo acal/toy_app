c952e7cb7239f0ffd462e24cf19109ac427a7c5b
Adam Calihman <adamcalihman@gmail.com>
2015-03-08 19:18:29 +0000

    further debug name validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :microposts
+  
+  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :username, presence: true, uniqueness: true
+   
 end
